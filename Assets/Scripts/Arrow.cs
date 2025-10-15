162026fe7d604eeab60ee64eb23c067ef634e277
0megq <nvatanshenas55@gmail.com>
2025-10-14 21:24:02 -0700

    Added 2 arrows as well as skybox and garden floor texture

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,10 +1,31 @@
+using TMPro;
 using UnityEngine;
 
 public class Arrow : Interactable
 {
+    public TextMeshPro text;
     public Transform player;
+
+    public void Awake()
+    {
+        text.enabled = false;
+    }
+    
     public override void Interact()
     {
-        player.position = transform.position + Vector3.up * 1;
+        player.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        player.rotation = transform.rotation;
+    }
+
+    public override void StartHover()
+    {
+        base.StartHover();
+        text.enabled = true;
+    }
+
+    public override void EndHover()
+    {
+        base.EndHover();
+        text.enabled = false;
     }
 }
